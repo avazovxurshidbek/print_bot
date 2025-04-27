1f02c9aa5d0f428961340831b8955049740a89ab
GitHubdagi_nicking <alicoder@gmail.com>
2025-04-27 22:29:12 +0500

    Bot kodini qo'shdim

--- bot.py
+++ bot.py
@@ -139,7 +139,7 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Main
 async def main():
-    app = ApplicationBuilder().token("SENING_BOT_TOKENING").build()
+    app = ApplicationBuilder().token("7591946515:AAFFMEgpPLkwRxADRCTlztIh0GxDdc1qLC8G").build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
